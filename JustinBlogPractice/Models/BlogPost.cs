44e15829fe3948ae45eb59311a8e017ed01e01f1
Justin Holsclaw <justin.holsclaw@ad.snow.edu>
2020-09-24 13:13:31 -0600

    first commit

---
+++ JustinBlogPractice/Models/BlogPost.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JustinBlogPractice.Models
+{
+    public class BlogPost
+    {
+        public string Title { get; set; }
+
+        public int ID { get; set; }
+        public string Body { get; set; }
+        public DateTime PostedOn { get; set; }
+        public DateTime EditedOn { get; set; }
+
+    }
+}
